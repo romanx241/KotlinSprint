fa37dd30652a669ed3dee0b57efd74bfeebfec31
romanx241 <hohlovr@mail.ru>
2024-11-25 18:50:44 +0300

    KS-1-1 refactor create smth

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_1
 
-fun main(){
-    val numberOfOrders : Int = 75
-    val answerForPurchase : String = "Благодарим за покупку в нашем магазине!"
+fun main() {
+    val numberOfOrders: Int = 75
+    val answerForPurchase: String = "Благодарим за покупку в нашем магазине!"
 }
