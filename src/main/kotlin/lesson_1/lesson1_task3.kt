d5060432fa56f08381331cfba893663f18410a8a
romanx241 <hohlovr@mail.ru>
2024-11-25 18:52:55 +0300

    KS-1-3 refactor boarding time

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,9 +1,9 @@
 package org.example.lesson_1
 
-fun main(){
-    val year : Int = 1961
-    var hour : String = "09"
-    var minute : String = "07"
+fun main() {
+    val year: Int = 1961
+    var hour: String = "09"
+    var minute: String = "07"
 
     println(year)
     println(hour)
