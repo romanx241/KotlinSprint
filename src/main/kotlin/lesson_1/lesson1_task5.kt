7becdaca39fa71d684cb99a636700db0f727eca0
romanx241 <hohlovr@mail.ru>
2024-11-26 22:16:14 +0300

    KS-1-5 refactor recalculation seconds

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,26 +1,15 @@
 package org.example.lesson_1
 
-import java.text.DecimalFormat
-import java.text.NumberFormat
-
 fun main() {
-    // общее количество секунд, проведенных Гагариным в космосе
     val secondsAll = 6480
-    // количество секунд в часе
     val secondHour = 3600
-    // количество минут в часе
     val minuteHour = 60
-    // количество целых часов
     val hours = (secondsAll / secondHour)
-    // остаток в секундах
     val secondRemains = (secondsAll - hours * secondHour)
-    // количество целых минут
     val minutes = (secondRemains / minuteHour)
-    // количество целых секунд
     val seconds = (secondRemains - minutes * minuteHour)
 
-    val f: NumberFormat = DecimalFormat("00")
-    println("[" + "${f.format(hours)} : ${f.format(minutes)} : ${f.format(seconds)}" + "]")
+    println("[" + "%02d:%02d:%02d".format(hours, minutes, seconds) + "]")
 
 }
 
