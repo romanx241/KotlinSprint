ae5b7cc34aeca51f718e8f73fdb2fb650dec8a26
romanx241 <hohlovr@mail.ru>
2024-11-28 00:29:07 +0300

    KS-3-2-firstLastName

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main() {
+    val firstName = "Татьяна"
+    val patronymic = "Сергеевна"
+    val lastName1 = "Андреева"
+    val lastName2 = "Сидорова"
+    val age1 = 20
+    val age2 = 22
+
+    println("[" + "$firstName $patronymic $lastName1, $age1" + "]")
+    println("[" + "$firstName $patronymic $lastName2, $age2" + "]")
+
+}
