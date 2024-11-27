ce8e261c823349873cbf0c75d08d38bd69789031
romanx241 <hohlovr@mail.ru>
2024-11-28 00:14:57 +0300

    KS-3-1-greeting

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_3
+
+fun main() {
+    val textGreeting = "Приветствую тебя"
+    val firstName = "Иван"
+    val day = "день"
+    val evening = "вечер"
+    val message1 = "$textGreeting $firstName! Сегодня ожидается хороший $day"
+    val message2 = "$textGreeting $firstName! Сегодня ожидается хороший $evening"
+
+    println(message1)
+    println(message2)
+}
+
+
+
