3268d1abc51fc3147a80bf4fafb8f3aab57fdea2
romanx241 <hohlovr@mail.ru>
2024-11-28 22:02:06 +0300

    KS-4-2-cargoCategory

---
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -0,0 +1,27 @@
+package org.example.lesson_4
+
+fun main() {
+    val category: String = "'Average'"
+    var weight: Int = 20
+    var volume: Int = 80
+
+    val resultAverage: Boolean = (weight > WEIGHT_AVERAGE1 && weight <= WEIGHT_AVERAGE2) && (volume < VOLUME_AVERAGE)
+
+    println("Груз с весом" + " $weight " + "кг" + " и объемом" + " $volume " + "л" + " соответствует категории "
+            + category + ":" + "$resultAverage")
+
+    weight = 50
+    volume = 100
+
+    println("Груз с весом" + " $weight " + "кг" + " и объемом" + " $volume " + "л" + " соответствует категории "
+            + category + ":" + "$resultAverage")
+
+}
+
+const val WEIGHT_AVERAGE1 = 35
+const val WEIGHT_AVERAGE2 = 100
+const val VOLUME_AVERAGE = 100
+
+
+
+
