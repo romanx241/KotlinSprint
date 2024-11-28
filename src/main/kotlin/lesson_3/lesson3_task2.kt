12b0427c5a20cff7f04daa264281d8c64a57e972
romanx241 <hohlovr@mail.ru>
2024-11-28 18:01:30 +0300

    KS-3-2-refactoring firstLastName

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -3,12 +3,10 @@ package org.example.lesson_3
 fun main() {
     val firstName = "Татьяна"
     val patronymic = "Сергеевна"
-    val lastName1 = "Андреева"
-    val lastName2 = "Сидорова"
-    val age1 = 20
-    val age2 = 22
-
-    println("[" + "$firstName $patronymic $lastName1, $age1" + "]")
-    println("[" + "$firstName $patronymic $lastName2, $age2" + "]")
-
+    var lastName = "Андреева"
+    var age = 20
+    println("[" + "$firstName $patronymic $lastName, $age" + "]")
+    lastName = "Сидорова"
+    age = 22
+    println("[" + "$firstName $patronymic $lastName, $age" + "]")
 }
