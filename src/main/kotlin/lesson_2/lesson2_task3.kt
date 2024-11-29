117a80e72d8c53a9d90357051fad4c129471812e
romanx241 <hohlovr@mail.ru>
2024-11-29 21:09:45 +0300

    KS-2-3-refactoring1-train

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -4,12 +4,13 @@ fun main() {
     val checkOutTimeHour = 9
     val checkOutTimeMinute = 39
     val timeTravel = 457
-    val minuteInHour = 60
-    val checkOutTimeHorInMinute = checkOutTimeHour * minuteInHour
+
+    val checkOutTimeHorInMinute = checkOutTimeHour * MINUTE_IN_HOUR
     val checkOutTimeAllMinute = checkOutTimeHorInMinute + checkOutTimeMinute
     val trainArrivalAllMinute = checkOutTimeAllMinute + timeTravel
-    val trainArrivalHour = trainArrivalAllMinute / minuteInHour
-    val trainArrivalMinute = trainArrivalAllMinute % minuteInHour
+    val trainArrivalHour = trainArrivalAllMinute / MINUTE_IN_HOUR
+    val trainArrivalMinute = trainArrivalAllMinute % MINUTE_IN_HOUR
 
     println("Поезд прибудет в " + "$trainArrivalHour" + " часов " + "$trainArrivalMinute " + "минуты")
 }
+const val MINUTE_IN_HOUR = 60
